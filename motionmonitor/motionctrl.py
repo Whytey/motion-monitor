db81abb9eee38b8cbad240e2d5be262f33854ba5
Whytey <Whytey@users.noreply.github.com>
2018-04-15 20:47:22 +1000

    skeleton of motion control

---
+++ motionmonitor/motionctrl.py
@@ -0,0 +1,37 @@
+
+
+class MotionThread():
+    """Represents a camera thread within motion.  Includes important parts of the configuration for a thread."""
+
+    def __init__(self):
+        pass
+
+    @property
+    def id(self):
+        pass
+
+    @property
+    def mjpeg_port(self):
+        pass
+
+
+
+
+class MotionController():
+    def __init__(self, mm):
+        threads = {}
+        pass
+
+    def is_running(self):
+        pass
+
+    def start(self):
+        pass
+
+
+    def stop(self):
+        pass
+
+    @property
+    def pid(self):
+        pass
