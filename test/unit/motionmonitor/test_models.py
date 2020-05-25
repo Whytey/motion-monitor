dc08fbb2df381099dae122c1db1adc605139dc94
Whytey <Whytey@users.noreply.github.com>
2020-05-25 22:32:17 +1000

    And fixed the build again :|

--- test/unit/motionmonitor/test_models.py
+++ test/unit/motionmonitor/test_models.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from motionmonitor.models import Camera, Frame, Event, EventFrame
 
@@ -18,30 +19,30 @@ class TestEvent(unittest.TestCase):
 
     def test_simple(self):
         # def __init__(self, event_id, camera_id, start_time):
-        e = Event(EVENT_ID, CAMERA_ID, "timestamp")
+        e = Event(EVENT_ID, CAMERA_ID, datetime.now())
         self.assertEqual(EVENT_ID, e.id)
         self.assertEqual(CAMERA_ID, e.camera_id)
         self.assertIsNone(e.top_score_frame)
 
     def test_get_json(self):
         import json
-        e = Event(EVENT_ID, CAMERA_ID, "timestamp")
+        e = Event(EVENT_ID, CAMERA_ID, datetime.now())
         json_str = e.to_json()
         # Check it can be parsed as JSON
         json_obj = json.dumps(json_str)
         self.assertIsNotNone(json_obj)
 
     def test_append_frame(self):
-        e = Event(EVENT_ID, CAMERA_ID, "timestamp")
+        e = Event(EVENT_ID, CAMERA_ID, datetime.now())
         self.assertIsNone(e.top_score_frame)
 
         # Add a frame to the event
-        ef1 = EventFrame(CAMERA_ID, EVENT_ID, "timestamp1", 0, "filename1", 100)
+        ef1 = EventFrame(CAMERA_ID, EVENT_ID, datetime.now(), 0, "filename1", 100)
         e.append_frame(ef1)
         self.assertEqual(ef1, e.top_score_frame)
 
         # Add a higher scoring frame to the event
-        ef2 = EventFrame(CAMERA_ID, EVENT_ID, "timestamp2", 1, "filename2", 200)
+        ef2 = EventFrame(CAMERA_ID, EVENT_ID, datetime.now(), 1, "filename2", 200)
         e.append_frame(ef2)
         self.assertEqual(ef2, e.top_score_frame)
         self.assertNotEqual(ef1, ef2)
@@ -73,12 +74,12 @@ class TestCamera(unittest.TestCase):
         self.assertIsNone(c.last_snapshot)
 
         # Add a snapshot frame
-        f1 = Frame(Camera, "timestamp1", 0, "filename1")
+        f1 = Frame(Camera, datetime.now(), 0, "filename1")
         c.append_snapshot_frame(f1)
         self.assertEqual(f1, c.last_snapshot)
 
         # Add another snapshot frame
-        f2 = Frame(Camera, "timestamp2", 1, "filename2")
+        f2 = Frame(Camera, datetime.now(), 1, "filename2")
         c.append_snapshot_frame(f2)
         self.assertEqual(f2, c.last_snapshot)
         self.assertNotEqual(f1, f2)
