f5080fa3b571c11db6daa494d9a691fbf645e826
Whytey <Whytey@users.noreply.github.com>
2020-06-22 22:19:18 +1000

    Minor cleanup of a test.

--- test/unit/motionmonitor/extensions/test_recorder_models.py
+++ test/unit/motionmonitor/extensions/test_recorder_models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import peewee as pw
 
 import motionmonitor
-import motionmonitor.extensions.recorder.models as db
+from motionmonitor.extensions.recorder import models
 from motionmonitor.extensions.recorder.models import Event
 
 CAMERA_ID = "1"
@@ -15,9 +15,8 @@ class EventTableTests(unittest.TestCase):
 
     def setUp(self) -> None:
         sqlite_mem = pw.SqliteDatabase(":memory:")
-        self.db = db
-        self.db.proxy.initialize(sqlite_mem)
-        sqlite_mem.create_tables([db.Event], safe=False)
+        models.proxy.initialize(sqlite_mem)
+        sqlite_mem.create_tables([Event], safe=False)
 
     def test_from_native(self):
         now = datetime.now()
@@ -35,7 +34,7 @@ class EventTableTests(unittest.TestCase):
         self.assertEqual(CAMERA_ID, native_event.camera_id)
         self.assertEqual(now, native_event.start_time)
 
-        events = self.db.Event.select()
+        events = Event.select()
         self.assertEqual(1, len(events))
 
 
